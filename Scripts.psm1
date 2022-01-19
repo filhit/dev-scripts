9fdd0f77d60074dbc8ab1ed80a0ff22a62dd3589
Andrey Rjeutski <filhit@gmail.com>
2022-01-19 15:14:29 +0100

    Add PruneMissingRemoteGitBranches

---
+++ Scripts.psm1
@@ -0,0 +1,7 @@
+function PruneMissingRemoteGitBranches {
+  git branch -v | Select-String gone |
+  ForEach-Object { $_ -replace "\s+([^\s]+)\s+.+", "$1" } |
+  ForEach-Object { git branch -D "$_" }
+}
+
+Export-ModuleMember -Function PruneMissingRemoteGitBranches
